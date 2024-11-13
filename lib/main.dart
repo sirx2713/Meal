b8a4220c15721326bf8a04106dfa543ecdd994a0
Tafadzwa Alexander Razaro <bradx2713@gmail.com>
2024-11-13 10:11:55 +0200

    Let us eat!

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sushi/landingview.dart';
+import 'package:sushi/mainview.dart';
 
 void main() {
   runApp(const MyApp());
@@ -33,7 +34,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Landingview(),
+      home: Mainview(),
     );
   }
 }
